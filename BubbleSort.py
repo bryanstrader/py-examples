80d3c95286c9db30a8821eea160032a622a18520
Bryan Strader <bpstrad@gmail.com>
2019-11-30 15:17:38 -0700

    bubble sort

---
+++ BubbleSort.py
@@ -0,0 +1,15 @@
+
+
+numbers = [0, 6, 12, 2, 3, 4, 5, 88, 34, 111, 23, 13, 99, 28]
+
+print("pre-sort: ", numbers)
+
+for n in numbers:
+	for i, n2 in enumerate(numbers):
+		if len(numbers) > i+1:
+			if n2 > numbers[i+1]:
+				temp = numbers[i+1]
+				numbers[i+1] = n2
+				numbers[i] = temp
+
+print("after-sort:", numbers)
