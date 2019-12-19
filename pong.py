7708dd0c9d23b51626bcccefafdc0880f24ddbbf
Bryan Strader <bpstrad@gmail.com>
2019-12-18 19:19:44 -0700

    clean up

--- pong.py
+++ pong.py
@@ -1,15 +1,20 @@
 import turtle
 import time
+import copy
 
+# game vars
+in_progress = True
+score_a = 0
+score_b = 0
 
-#view setup
+# view setup
 wn = turtle.Screen()
 wn.title("Pong")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
-#paddle setup
+# paddle setup
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
@@ -18,12 +23,7 @@ paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
-paddle_b = turtle.Turtle()
-paddle_b.speed(0)
-paddle_b.shape("square")
-paddle_b.color("white")
-paddle_b.shapesize(stretch_wid=5, stretch_len=1)
-paddle_b.penup()
+paddle_b = paddle_a.clone()
 paddle_b.goto(350, 0)
 
 # ball setup
@@ -44,48 +44,41 @@ def paddle_up(paddle):
     y += 20
     paddle.sety(y)
 
-
 def paddle_down(paddle):
     y = paddle.ycor()
     y -= 20
     paddle.sety(y)
 
-
 def paddle_up_a():
     paddle_up(paddle_a)
-
 
 def paddle_up_b():
     paddle_up(paddle_b)
 
-
 def paddle_down_a():
     paddle_down(paddle_a)
-
 
 def paddle_down_b():
     paddle_down(paddle_b)
 
-
 def update_score():
-    score = "Score: A - " + str(score_a) + " B - " + str(score_b)
-    wn.title("Pong (" + score + ")")
+    score = "Score: A - {0} B - {1}".format(score_a, score_b)
+    wn.title("Pong ({0})".format(score))
 
+def player_score():
+    update_score()
+    ball.goto(0, 0)
+    time.sleep(1)
+    ball.dx *= -1
 
 # Key bindings
 wn.listen()
 wn.onkeypress(paddle_up_a, "w")
 wn.onkeypress(paddle_down_a, "s")
-
 wn.onkeypress(paddle_up_b, "Up")
 wn.onkeypress(paddle_down_b, "Down")
 
-
 # Main game loop
-in_progress = True
-score_a = 0
-score_b = 0
-
 while in_progress:
     wn.update()
 
@@ -104,17 +97,11 @@ while in_progress:
 
     if ball.xcor() > 390:
         score_a += 1
-        update_score()
-        ball.goto(0, 0)
-        time.sleep(1)
-        ball.dx *= -1
+        player_score()
 
     if ball.xcor() < -390:
         score_b += 1
-        update_score()
-        ball.goto(0, 0)
-        time.sleep(1)
-        ball.dx *= -1
+        player_score()
 
     # detect paddle collision
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 50):
