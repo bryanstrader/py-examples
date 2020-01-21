534877b12a3f478317e4ae721fbfe8abcd4f131a
Bryan Strader <bpstrad@gmail.com>
2020-01-20 19:23:00 -0700

    small changes

--- webscraping.py
+++ webscraping.py
@@ -11,3 +11,8 @@ driver.get('https://phoenix.craigslist.org/')
 forsale = driver.find_element_by_xpath('''//*[@id="sss"]/h4/a''')
 
 forsale.click()
+
+forsale_items = driver.find_elements_by_class_name('result-title')
+
+for item in forsale_items:
+    print(item.text)
